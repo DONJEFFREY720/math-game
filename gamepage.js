97ceeeef62494ce9b7827f9ed3b462ba417eab14
DON JEFFREY <95631932+DONJEFFREY720@users.noreply.github.com>
2023-02-24 20:56:35 +0530

    Update gamepage.js

--- gamepage.js
+++ gamepage.js
@@ -1,1 +1,16 @@
+function send(){
 
+number1 = document.getElementById("questuon_input1").value
+number2 = document.getElementById("questuon_input2").value
+actual_answer = praseInt(number1) *praseInt(number2)
+question_number= "<h4>"+number1+"X"+number2+"</h4>"
+input_box = "<br>ANSWER:<input type='text' id='input_check_box'>";
+check_button =  "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+row = question_number+input_box+check_button
+
+document.getElementById("output").innerHTML = row;
+document.getElementById("questuon_input1").value="";
+document.getElementById("questuon_input2").value="";
+
+
+}
