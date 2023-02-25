1d78a8a2b7af5d146945235ebf19ea9a712e1829
DON JEFFREY <95631932+DONJEFFREY720@users.noreply.github.com>
2023-02-25 11:43:07 +0530

    Update gamepage1.js

--- gamepage1.js
+++ gamepage1.js
@@ -14,7 +14,7 @@ function send(){
 
 number1 = document.getElementById("questuon_input1").value
 number2 = document.getElementById("questuon_input2").value
-actual_answer = praseInt(number1) *praseInt(number2)
+actual_answer = parseInt(number1) *parseInt(number2)
 question_number= "<h4>"+number1+"X"+number2+"</h4>"
 input_box = "<br>ANSWER:<input type='text' id='input_check_box'>";
 check_button =  "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
