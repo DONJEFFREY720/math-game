91bf3517c5d36cdbd35c19099452e20460c85cbd
DON JEFFREY <95631932+DONJEFFREY720@users.noreply.github.com>
2023-02-27 19:20:46 +0530

    Update gamepage1.js

--- gamepage1.js
+++ gamepage1.js
@@ -2,6 +2,8 @@ player1_name = localStorage.getItem("user1");
 player2_name = localStorage.getItem("user2");
 player1_score = 0;
 player2_score = 0;
+answer_TURN = "player2"
+question_TURN ="player1";
 
 document.getElementById("player1_name").innerHTML = player1_name + " : ";
 document.getElementById("player2_name").innerHTML = player2_name + " : ";
@@ -26,3 +28,45 @@ document.getElementById("questuon_input2").value="";
 
 
 }
+
+function check{
+
+        answer_player=document.getElementById("input_check_box").value
+            if(actual_answer==answer_player){
+                if(answer_TURN=="player1"){
+                    player1_score = player1_score+1
+                    document.getElementById("player1_score").innerHTML=player1_score
+                }
+
+          else{
+            player2_score = player2_score+1
+            document.getElementById("player2_score").innerHTML=player2_score
+          }
+
+          if(question_TURN=="player1"){
+          question_TURN = "player2"
+          document.getElementById("player_question").innerHTML =  "Question Turn - " + player2_name;
+            }
+
+           else{
+            question_TURN = "player1"
+            document.getElementById("player_question").innerHTML =  "Question Turn - " + player1_name;
+        }
+
+        if(answer_TURN=="player2"){
+            answer_TURN = "player1"
+            document.getElementById("player_answer").innerHTML =  "Answer Turn - " + player1_name;
+        }
+
+       else{
+            answer_TURN = "player2"
+            document.getElementById("player_answer").innerHTML =  "Answer Turn - " + player2_name;
+        }
+              }
+     document.getElementById("output").innerHTML=""
+            }
+}
+
+
+
+
